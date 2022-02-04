8ae9e158460e92ab537851ba9674038997f64b7f
Arjunraj Alora <arjunrajaa@gmail.com>
2022-02-04 21:50:37 +0000

    setup ui for weather display

--- WeatherForecastSpa/ClientApp/src/app/app.module.ts
+++ WeatherForecastSpa/ClientApp/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+//import { MatButtonModule } from '@angular/material/button';
+//import { MatListModule } from '@angular/material/list';
+//import { MatCardModule } from '@angular/material/card';
+//import { MatInputModule } from '@angular/material/input';
+//import { MatTabsModule } from '@angular/material/tabs';
+//import { MatToolbarModule } from '@angular/material/toolbar';
+//import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -22,6 +29,13 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    //MatButtonModule,
+    //MatToolbarModule,
+    //MatInputModule,
+    //MatTabsModule,
+    //MatCardModule,
+    //MatDividerModule,
+    //MatListModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- WeatherForecastSpa/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ WeatherForecastSpa/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,53 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
+  styleUrls: ['./fetch-data.component.css']
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  loc: any;
+  currentWeather: any = <any>{};
+  forecastWeather: any = <any>{};
+  msg!: string;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+  constructor(http: HttpClient, private weatherService: WeatherService, @Inject('BASE_URL') baseUrl: string) {
+    //this.searchWeather(this.loc);
+    }
+    ngOnInit() {
+    }
+  searchWeather() {
+      this.msg = '';
+      this.currentWeather = {};
+      this.forecastWeather = {};
+      this.weatherService.getCurrentWeather(this.loc)
+        .subscribe(res => {
+          this.currentWeather = res;
+        }, err => {
+          if (err.error && err.error.message) {
+            alert(err.error.message);
+            this.msg = err.error.message;
+            return;
+          }
+          alert('Failed to get weather.');
+        }, () => {
+        })
+      this.weatherService.getForecast(this.loc)
+        .subscribe(res => {
+          this.forecastWeather = res;
+        }, err => {
+          if (err.error && err.error.message) {
+            alert(err.error.message);
+            this.msg = err.error.message;
+            return;
+          }
+          alert('Failed to get forecasted weather.');
+        }, () => {
+        })
+    }
+    resultFound() {
+      return Object.keys(this.currentWeather).length > 0;
+    }
 }
