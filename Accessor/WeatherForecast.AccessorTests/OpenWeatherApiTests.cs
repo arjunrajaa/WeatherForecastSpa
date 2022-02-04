0821cad1d515df459c59da608c291f7b85ffb164
Arjunraj Alora <arjunrajaa@gmail.com>
2022-02-04 19:45:37 +0000

    Addding tests for accessor

---
+++ Accessor/WeatherForecast.AccessorTests/OpenWeatherApiTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherForecast.Accessor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherForecast.Accessor.Tests
+{
+    [TestClass()]
+    public class OpenWeatherApiTests
+    {
+        
+        [TestMethod()]
+        public void ShoulReturnOutput()
+        {
+            var weatherRepo = new OpenWeatherApi();
+            var obj = weatherRepo.GetWeatherInfo("Coventry").GetAwaiter().GetResult();
+            Assert.IsNotNull(obj);
+        }
+    }
+}
