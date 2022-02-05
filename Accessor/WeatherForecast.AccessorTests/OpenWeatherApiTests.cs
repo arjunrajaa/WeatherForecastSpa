4c0f8ae101b03dddda9e60da8e241d25714a9c64
Arjunraj Alora <arjunrajaa@gmail.com>
2022-02-05 13:47:13 +0000

    Fix Test

--- Accessor/WeatherForecast.AccessorTests/OpenWeatherApiTests.cs
+++ Accessor/WeatherForecast.AccessorTests/OpenWeatherApiTests.cs
@@ -3,19 +3,32 @@ using WeatherForecast.Accessor;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace WeatherForecast.Accessor.Tests
 {
     [TestClass()]
     public class OpenWeatherApiTests
     {
-        
+        private readonly IConfiguration _configuration;
+        public OpenWeatherApiTests()
+        {
+            var myConfiguration = new Dictionary<string, string>
+                                {
+                                    {"OpenWeather:BaseUrl", "http://api.openweathermap.org/data/2.5/"},
+                                    {"OpenWeather:ApiKey", "5ce4f9d7b705a8d978faff161fe15265"}
+                                };
+
+            _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+        }
         [TestMethod()]
         public void ShoulReturnOutput()
         {
-            var weatherRepo = new OpenWeatherApi();
-            var obj = weatherRepo.GetWeatherInfo("Coventry").GetAwaiter().GetResult();
-            Assert.IsNotNull(obj);
+            var weatherRepo = new OpenWeatherApi(_configuration);
+            var weather = weatherRepo.GetWeatherInfo("Coventry").GetAwaiter().GetResult();
+            Assert.IsNotNull(weather);
         }
     }
 }
